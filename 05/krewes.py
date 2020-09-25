f063936454d4612522660724f94dc36fe702459f
Arib <aribchowdhury03@gmail.com>
2020-09-25 13:15:21 -0400

    Uploading what I think I was supposed to do for assignment 5

---
+++ 05/krewes.py
@@ -0,0 +1,13 @@
+import random as r
+
+teamname = str(input("Choose a Team (orpheus, rex, or endymion): ")).lower()
+while (teamname not in ["orpheus", "rex", "endymion"]):
+    teamname = str(input("Please Input One Of The Following Options (orpheus, rex, or endymion): ")).lower()
+
+KREWES = {
+    'orpheus': ['ERIC', 'SAUVE', 'JONATHAN', 'PAK', 'LIAM', 'WINNIE', 'KELLY', 'JEFFREY', 'KARL', 'ISHITA', 'VICTORIA', 'BENJAMIN', 'ARIB', 'AMELIA', 'CONSTANCE', 'IAN'],
+    'rex': ['ANYA', 'DUB-Y', 'JESSICA', 'ALVIN', 'HELENA', 'MICHELLE', 'SHENKER', 'ARI', 'STELLA', 'RENEE', 'MADELYN', 'MAC', 'RYAN', 'DRAGOS'],
+    'endymion': ['JASON', 'DEAN', 'MADDIE', 'SAQIF', 'CINDY', 'YI LING', 'RUOSHUI', 'FB', 'MATTHEW', 'MAY', 'ERIN', 'MEIRU']
+}
+
+print(r.choice(KREWES[teamname]))
