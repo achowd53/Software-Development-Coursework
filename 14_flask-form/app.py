cc565c0c7021713c5c5624c849bcb2ec48f415b4
aribchowdhury <58579404+aribchowdhury@users.noreply.github.com>
2020-12-10 08:10:59 -0500

    Update app.py

--- 14_flask-form/app.py
+++ 14_flask-form/app.py
@@ -1,3 +1,8 @@
+# Team Greedy Toads: Helena Williams, Arib Chowdhury, Benjamin Gallai, Anya Zorin
+# SoftDev
+# K14: Form and Function
+# 2020-12-08
+
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
@@ -7,7 +12,7 @@ from flask import request           #facilitate form submission
 
 app = Flask(__name__)    #create Flask object
 
-
+#Starter Code
 @app.route("/") #, methods=['GET', 'POST'])
 def disp_loginpage():
     print("\n\n\n")
@@ -23,7 +28,7 @@ def disp_loginpage():
     print(request.headers)
     return render_template( 'login.html' )
 
-
+#Response rendering and form request using GET
 @app.route("/auth") # , methods=['GET', 'POST'])
 def authenticate():
     print("\n\n\n")
@@ -37,7 +42,10 @@ def authenticate():
     #print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
+    #variable to store response in
     fullResp = ""
+    #gets username and sends it using render templates and displays using jinja2
+    #the three different responses are for Mario, Luigi, and any other username
     if(request.args.get("username")=="Mario"):
         fullResp+="Hello, Mario! Waaaa hooo HAAAH! This was a get request!"  #response to a form submission
         return render_template('response.html',response=fullResp,leResponsePage="la de daaaaaaaa")
