8fe5d831ffa08c8dc5d8c1df0052a67bfc1cc258
Arib <aribc@mit.edu>
2020-12-14 20:53:18 -0500

    Added comments (bad but there)

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -13,13 +13,13 @@ db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
 #==========================================================
-def file(filename):
-    file = csv.DictReader(open(filename))
-    name = filename.split(".")[0]
-    c.execute("CREATE TABLE "+name+"("+", ".join(file.fieldnames)+")\n")
+def file(filename): #Function to read in each csv into the SQLite database
+    file = csv.DictReader(open(filename)) #Opening csv and reading it through DictReader
+    name = filename.split(".")[0] #Name of table is the filename removing the extension
+    c.execute("CREATE TABLE "+name+"("+", ".join(file.fieldnames)+")\n") #Creating table with same columns as csv
     for row in file:
-        VALUES = ", ".join(value if value.isnumeric() else '"'+value+'"' for value in row.values())
-        c.execute("INSERT INTO "+name+" VALUES(%s);" % VALUES)
+        VALUES = ", ".join(value if value.isnumeric() else '"'+value+'"' for value in row.values()) #If the value is supposed to be nummeric, make it represented as such otherwise its a string
+        c.execute("INSERT INTO "+name+" VALUES(%s);" % VALUES) #Insert the values into the table for each row
 file("students.csv")
 file("courses.csv")
 # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
